3d145b26e65181a2a8437939a26d66bb2f154019
Emard <vordah@gmail.com>
2022-04-07 20:04:28 +0200

    radwag app25.3y support files

---
+++ debian/usr/share/autoinput/balance-radwag-app25-3y.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# cat /usr/local/bin/autoinput.sh
+sleep 1
+
+exec /usr/bin/autoinput --device=/dev/ttyUSB.RADWAG-APP25.3Y \
+  --protocol=radwag --bps=4800 --parity=none --stopbits=1 \
+  --pcspeaker="aplay -q /usr/share/sounds/sound-icons/beginning-of-line &"
